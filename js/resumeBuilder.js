e7b4d065dd6a273d67f8d3be6c1c2f510bf9bbbb
Theerasak Boonprajam <Zakk@Theerasaks-MacBook-Pro.local>
2016-04-30 11:05:16 +0700

    change js file and css and html for resubmit project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,173 +1,157 @@
 var bio = {
-    "name" : "Theerasak Boonprajam",
-    "role" : "Front-end web developer",
-    "welcomeMessage" : "Hello welcome to my portfolio website",
-    "contacts" : {
-        "mobile" : "+66 84 690 3844",
-        "email" : "zakk@zakk.me",
-        "github" : "zakky82",
-        "twitter" : "@zakkusm",
-        "location" : "Koh Samui"
-        },
-    "skills" : ["HTML", "CSS", "JavaScript", "Adobe Illustrator", "Photoshop"],
-    "bioPic" : "images/profilepic.jpg"
+    "name": "Theerasak Boonprajam",
+    "role": "Front-end web developer",
+    "contacts": {
+        "mobile": "+66 84 690 3844",
+        "email": "zakk@zakk.me",
+        "github": "zakky82",
+        "twitter": "@zakkusm",
+        "location": "Koh Samui"
+    },
+    "welcomeMessage": "Hello welcome to my portfolio website",
+    "skills": ["HTML", "CSS", "JavaScript", "Adobe Illustrator", "Photoshop"],
+    "bioPic": "http://s32.postimg.org/jq7jzrwk5/profilepic.jpg"
 };
 
 var education = {
-    "schools" : [{
-        "name" : "Chiang Mai University",
-        "location" : "Chiang Mai, Thailand",
-        "degree" : "BSc",
-        "major" : "Radiologic Technology",
-        "dates" : "2000",
-        "url" : "http://www.cmu.ac.th"
-    },
-    {
-        "name" : "Saint Martin Colleage of Art and Design",
-        "location" : "London",
-        "degree" : "BA",
-        "major" : "Fashion Design Honour",
-        "dates" : "2010",
-        "url" : "http://www.smca.ac.uk"
+    "schools": [{
+        "name": "Chiang Mai University",
+        "location": "Chiang Mai, Thailand",
+        "degree": "BSc",
+        "majors": ["Radiologic Technology", "Physical Medical Science"],
+        "dates": "2000 - 2004",
+        "url": "http://www.cmu.ac.th"
+    }, {
+        "name": "Saint Martin Colleage of Art and Design",
+        "location": "London",
+        "degree": "BA",
+        "majors": ["Fashion Design Honour", "Fashion Merchandising"],
+        "dates": "2005 - 2010",
+        "url": "http://www.smca.ac.uk"
     }],
-    "onlineCourses" : [{
-        "title" : "Front-end Web Development, Nanodegree",
-        "school" : " Udacity",
-        "dates" : "February 2016",
-        "url" : "http://www.udacity.com"
+    "onlineCourses": [{
+        "title": "Front-end Web Development, Nanodegree",
+        "school": "Udacity",
+        "date": "2016",
+        "url": "http://www.udacity.com"
     }]
 };
 
 var work = {
-    "jobs" : [{
-        "employer" : "Samui Emerald Condominium",
-        "title" : "Web Development",
-        "location" : "Bophut, Koh Samui",
-        "dates" : "2011-2015",
-        "description" : "Create website for the condominum to help the client communicate with their client and as a hub for booking and checking pregression of the condo."
-        },
-        {
-        "employer" : "Property Research Thailand",
-        "title" : "Web Design and Development",
-        "location" : "Koh Samui,Thailand",
-        "dates" : "2012-2014",
-        "description" : "Create and develop website for customer to be able to their real estate and communicate with customer."
-        }]
+    "jobs": [{
+        "employer": "Samui Emerald Condominium",
+        "title": "Web Development",
+        "location": "Bophut, Koh Samui",
+        "dates": "2011-2015",
+        "description": "Create website for the condominum to help the client communicate with their client and as a hub for booking and checking pregression of the condo."
+    }, {
+        "employer": "Property Research Thailand",
+        "title": "Web Design and Development",
+        "location": "Koh Samui,Thailand",
+        "dates": "2012 - 2014",
+        "description": "Create and develop website for customer to be able to their real estate and communicate with customer."
+    }]
 };
 
 var projects = {
-    "projects" : [{
-        "title" : "Sample Project 1",
-        "dates" : "2014",
-        "description" : "When I was 28 I fall in love with a guy who is came to me as a guest. The first time I lay my eyes on him I feel like the whole world is blur and like feeling in the fantasy world.",
-        "images" : ["images/197x148.gif","images/197x148.gif"]
-        }]
+    "projects": [{
+        "title": "Sample Project 1",
+        "dates": "2014",
+        "description": "Resigned because sent alas jeez the and much after more groaned some responsible therefore poured convenient flapped iguanodon musically along.",
+        "images": ["images/197x148.gif", "images/197x148.gif"]
+    }]
 };
 
-//Formatted Name and Role and prepend to the <h1>
+//Display Bio using modular function
 
-//Because prepend, what added last is come first, so start from the back
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+bio.display = function() {
+
+    //Formatted Name and Role and prepend to the <h1>
+
+    //Because prepend, what added last is come first, so start from the back
+
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
     $("#header").prepend(formattedRole);
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
     $("#header").prepend(formattedName);
 
-// Display Contact
-//$("#topContacts").append(HTMLcontactGeneric);
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    $("#topContacts, #footerContacts").append(formattedMobile);
 
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-$("#topContacts, #footerContacts").append(formattedMobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#topContacts, #footerContacts").append(formattedEmail);
 
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-$("#topContacts, #footerContacts").append(formattedEmail);
+    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    $("#topContacts, #footerContacts").append(formattedTwitter);
 
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-$("#topContacts, #footerContacts").append(formattedTwitter);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#topContacts, #footerContacts").append(formattedGithub);
 
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-$("#topContacts, #footerContacts").append(formattedGithub);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts, #footerContacts").append(formattedLocation);
 
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-$("#topContacts, #footerContacts").append(formattedLocation);
+    var formattedwelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(formattedwelcomeMessage);
 
- var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
-   $("#header").append(formattedPic);
+    var formattedImage = HTMLbioPic.replace("%data%", bio.bioPic);
+    $("#header").append(formattedImage);
 
-var formattedwelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-$("#header").append(formattedwelcomeMessage);
+    // Display header and skill in Bio
+    $("#header").append(HTMLskillsStart);
 
-
-// Display header and skill in Bio
-
-if(bio.skills.length > 0) {
-   $("#header").append(HTMLskillsStart);
-
-   var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-   $("#skills").append(formattedSkill);
-
-   formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-
-   formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-
-   formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-    $("#skills").append(formattedSkill);
-
-   formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
-   $("#skills").append(formattedSkill);
-
+    for (var i = 0, length = bio.skills.length; i < length; i++) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+    }
 }
 
-
+bio.display();
 
 // Display work
+// move the work into function
 
-// move the workj into function
-function displayWork() {
+work.display = function() {
+    work.jobs.forEach(function(job) {
+        //create new div for work experience
+        $("#workExperience").append(HTMLworkStart);
+        // concat employer and title
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
 
-for (job in work.jobs) {
-    // create new div for work experience
-    $("#workExperience").append(HTMLworkStart);
-    // concat employer and title
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
 
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
 
-    $(".work-entry:last").append(formattedEmployerTitle);
+        var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+        $(".work-entry:last").append(formattedDates);
 
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    $(".work-entry:last").append(formattedDates);
+        var formattedLocation = HTMLworkLocation.replace("%data%", job.location);
+        $(".work-entry:last").append(formattedLocation);
 
-    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-    $(".work-entry:last").append(formattedLocation);
+        var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
+        $(".work-entry:last").append(formattedDescription);
 
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    $(".work-entry:last").append(formattedDescription);
+    });
+}
 
-    }
-};
-
-displayWork();
-
+work.display();
 // collect clicks locations
 
 $(document).click(function(loc) {
     var x = loc.pageX;
     var y = loc.pageY;
 
-    logClicks(x,y);
+    logClicks(x, y);
 });
 
 function locationizer(work_obj) {
     var locationArray = [];
 
     for (job in work_obj.jobs) {
-    var newLocation = work_obj.jobs[job].location;
-    locationArray.push(newLocation);
+        var newLocation = work_obj.jobs[job].location;
+        locationArray.push(newLocation);
     }
     return locationArray;
 }
@@ -209,7 +193,7 @@ education.display = function() {
         var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
         var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
         var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
         var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
         $(".education-entry:last").append(formattedName);
         $(".education-entry:last").append(formattedDates);
@@ -217,7 +201,7 @@ education.display = function() {
         $(".education-entry:last").append(formattedMajor);
         $(".education-entry:last").append(formattedDegree);
     }
-        $("#education").append(HTMLonlineClasses);
+    $("#education").append(HTMLonlineClasses);
 
     for (school in education.onlineCourses) {
         $("#education").append(HTMLschoolStart);
@@ -225,7 +209,7 @@ education.display = function() {
         $(".education-entry:last").append(formattedTitle);
         var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[school].school);
         $(".education-entry:last").append(formattedSchool);
-        var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[school].dates);
+        var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[school].date);
         $(".education-entry:last").append(formattedDates);
         var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[school].url);
         $(".education-entry:last").append(formattedURL);
@@ -237,25 +221,3 @@ education.display();
 //Display Maps
 
 $("#mapDiv").append(googleMap);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
